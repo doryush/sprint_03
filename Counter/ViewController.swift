728ef75810b0a0064cf5768231d69ab20086ec14
doryush <normatov_d@auca.kg>
2024-11-13 20:54:56 +0600

    basic project has been completed

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,18 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    
     @IBOutlet weak var backgroundImage: UIImageView!
     
     @IBOutlet weak var incrementButton: UIButton!
     
     @IBOutlet weak var numberOfBooks: UILabel!
     
+    private var numberOfBooksCount: Int = 0 {
+        didSet {
+            numberOfBooks.text = "\(numberOfBooksCount)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Проект Создан")
-        // Do any additional setup after loading the view.
+        
+        incrementButton.layer.cornerRadius = incrementButton.frame.height / 2
     }
-
-
+    
+    
+    @IBAction func incrementButtonTapped(_ sender: Any) {
+        numberOfBooksCount += 1
+    }
 }
+
 
