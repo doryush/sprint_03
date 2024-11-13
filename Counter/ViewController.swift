3d1a840ae75fccd5d976bc98582916f05904f406
doryush <normatov_d@auca.kg>
2024-11-13 19:38:13 +0600

    Создан проект

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Проект Создан")
         // Do any additional setup after loading the view.
     }
 
